552d216549615f1e1c4324e28e8bb73939b274d4
Anders Albert <60234212+doctrino@users.noreply.github.com>
2023-07-15 15:31:15 +0200

    fix: Set config as classvar (#39)
    * fix: Set config as classvar
    
    * refactor: update examples
    
    * refactor: added script for updating version
    
    * build: bump

---
+++ scripts/update_version.py
@@ -0,0 +1,26 @@
+"""
+This is a convince script to update the version number in the package.
+"""
+from pathlib import Path
+
+REPO_ROOT = Path(__file__).parent.parent.resolve()
+EXAMPLES_DIR = REPO_ROOT / "examples"
+
+
+def main():
+    last_version = "0.12.1"
+    new_version = "0.12.2"
+
+    pyproject_toml = REPO_ROOT / "pyproject.toml"
+    version_py = REPO_ROOT / "cognite" / "pygen" / "_version.py"
+    api_client_files = list(EXAMPLES_DIR.glob("**/_api_client.py"))
+
+    for file in [pyproject_toml, version_py] + api_client_files:
+        content = file.read_text().replace(last_version, new_version)
+        file.write_text(content)
+        print(f"Updated {file.relative_to(REPO_ROOT)}, replacing {last_version} with {new_version}.")
+    print("Done")
+
+
+if __name__ == "__main__":
+    main()
