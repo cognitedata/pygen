f734435a80bc9a774a27866308ad2f4b7114db51
anders-albert <anders0albert@gmail.com>
2023-07-16 08:31:16 +0200

    refactor: Upgraded script to now overwrite date_transformations_pairs

--- scripts/generate_market_sdk.py
+++ scripts/generate_market_sdk.py
@@ -1,6 +1,9 @@
+from pathlib import Path
+
 from cognite.client import CogniteClient
 
-from cognite import pygen
+from cognite.pygen import write_sdk_to_disk
+from cognite.pygen._generator import MultiModelSDKGenerator, _load_data_model
 from movie_domain.client import MovieClient
 from tests.constants import examples_dir
 
@@ -8,14 +11,19 @@ from tests.constants import examples_dir
 def main():
     client = MovieClient.from_toml("config.toml")
     c: CogniteClient = client.persons._client
-    pygen.generate_multimodel_sdk(
-        c,
-        model_ids=[("market", "CogPool", "3"), ("market", "PygenPool", "3")],
-        top_level_package="markets.client",
-        client_name="MarketClient",
-        output_dir=examples_dir,
-        logger=print,
-    )
+    data_models = _load_data_model(c, [("market", "CogPool", "3"), ("market", "PygenPool", "3")], print)
+
+    top_level_package = "markets.client"
+    sdk_generator = MultiModelSDKGenerator(top_level_package, "MarketClient", data_models, print)
+
+    sdk = sdk_generator.generate_sdk()
+    # Remove the date_transformation_pair as this is expected to be set manually
+    client_dir = Path(top_level_package.replace(".", "/"))
+    sdk.pop(client_dir / "data_classes" / "_date_transformation_pairs.py")
+    sdk.pop(client_dir / "_api" / "date_transformation_pairs.py")
+
+    write_sdk_to_disk(sdk, examples_dir)
+    print("Market SDK Created!")
 
 
 if __name__ == "__main__":
