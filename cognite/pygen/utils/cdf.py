46b8ae36814e91c57905e5c144e70aba2a7db3e1
anders-albert <anders0albert@gmail.com>
2023-08-14 07:30:22 +0200

    docs: fix mistake in load_from_toml

--- cognite/pygen/utils/cdf.py
+++ cognite/pygen/utils/cdf.py
@@ -13,7 +13,8 @@ def load_cognite_client_from_toml(
 
     Args:
         toml_file: Path to toml file
-        section: Name of the section in the toml file to use. If None, use the "cognite" level of the toml file.
+        section: Name of the section in the toml file to use. If None, use the top level of the toml file.
+                 Defaults to "cognite".
 
     Returns:
         A CogniteClient with configurations from the toml file.
