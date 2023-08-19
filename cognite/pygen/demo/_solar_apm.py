6e1e15fbcebfee09aa52a020fdd2df1683556332
anders-albert <anders0albert@gmail.com>
2023-08-19 16:30:52 +0200

    feat: Population of CDF resources

--- cognite/pygen/demo/_solar_apm.py
+++ cognite/pygen/demo/_solar_apm.py
@@ -1,17 +1,54 @@
 from __future__ import annotations
 
+import csv
 import pathlib
-from typing import Any, Callable, Optional
+from collections import defaultdict
+from collections.abc import Sequence
+from datetime import datetime
+from typing import Any, Callable, Optional, Protocol, Union, cast, get_args, get_origin, get_type_hints
 
 from cognite.client import CogniteClient
-from cognite.client.data_classes.data_modeling import DataModel, MappedProperty, SpaceApply
-from cognite.client.data_classes.data_modeling.ids import DataModelId, ViewId
+from cognite.client._api.files import FilesAPI
+from cognite.client.data_classes import FileMetadata, FileMetadataList, TimeSeries, TimeSeriesList
+from cognite.client.data_classes._base import (
+    CogniteResource,
+    T_CogniteResource,
+    T_CogniteResourceList,
+)
+from cognite.client.data_classes.data_modeling import (
+    DataModel,
+    DirectRelationReference,
+    EdgeApply,
+    EdgeApplyResultList,
+    MappedProperty,
+    NodeApply,
+    NodeApplyResultList,
+    NodeOrEdgeData,
+    SingleHopConnectionDefinition,
+    SpaceApply,
+    View,
+    data_types,
+)
+from cognite.client.data_classes.data_modeling.ids import ContainerId, DataModelId
+from cognite.client.utils._text import to_snake_case
 
 from cognite.pygen import generate_sdk_notebook
+from cognite.pygen.demo._constants import DEFAULT_SPACE
 
-from ._constants import DEFAULT_SPACE
+_DATA_FOLDER = pathlib.Path(__file__).parent / "solar_apm_data"
 
-_DATA_FOLDER = pathlib.Path(__file__).parent / "apm_data"
+
+class CogniteCoreResourceAPI(Protocol[T_CogniteResourceList]):
+    def retrieve_multiple(
+        self,
+        ids: Optional[Sequence[int]] = None,
+        external_ids: Optional[Sequence[str]] = None,
+        ignore_unknown_ids: bool = False,
+    ) -> T_CogniteResourceList:
+        ...
+
+    def create(self, items: T_CogniteResourceList) -> T_CogniteResourceList:
+        ...
 
 
 class SolarAPM:
@@ -28,6 +65,7 @@ class SolarAPM:
         self._graphql = (_DATA_FOLDER / "model.graphql").read_text()
         self._data_model_id = DataModelId(space=space, external_id=model_external_id, version=model_version)
         self._echo: Callable[[str], None] = print
+        self._data_model: DataModel[View] | None = None
 
     def display(self):
         """
@@ -57,12 +95,12 @@ class SolarAPM:
         Returns:
             An instantiated SDK client for the APM model.
         """
-        self.deploy(client)
+        self._data_model = self.deploy(client)
         if populate:
             self.populate(client)
         return self.generate_sdk(client)
 
-    def deploy(self, client: CogniteClient) -> DataModel[ViewId]:
+    def deploy(self, client: CogniteClient) -> DataModel[View]:
         """
         Deploy the APM model to the CDF project the client is connected to.
 
@@ -82,7 +120,7 @@ class SolarAPM:
             space = client.data_modeling.spaces.apply(space_apply)
             self._echo(f"Created space {space.space}")
 
-        retrieved = client.data_modeling.data_models.retrieve(self._data_model_id)
+        retrieved = client.data_modeling.data_models.retrieve(self._data_model_id, inline_views=True)
         if retrieved:
             self._echo(f"Data model {self._data_model_id} already exists, skipping deployment")
             return retrieved.latest_version()
@@ -94,10 +132,13 @@ class SolarAPM:
             description="This data model was created by pygen for demo purposes.",
         )
         self._echo(f"Deployed data model {self._data_model_id}")
-        return client.data_modeling.data_models.retrieve(self._data_model_id).latest_version()
+        return client.data_modeling.data_models.retrieve(self._data_model_id, inline_views=True).latest_version()
 
     def populate(self, client: CogniteClient):
-        ...
+        if self._data_model is None:
+            raise ValueError("Cannot populate model before deploying it, please call deploy() first")
+
+        CSVLoader(_DATA_FOLDER, self._echo, None, self._data_model.space)
 
     def generate_sdk(self, client: CogniteClient) -> Any:
         """
@@ -152,35 +193,274 @@ class SolarAPM:
             self._echo(f"Deleted space {self._data_model_id.space}")
 
 
-class _Populator:
+class CSVLoader:
     def __init__(
         self,
         source_dir: pathlib.Path,
         echo: Optional[Callable[[str], None]] = None,
         data_set_id: int | None = None,
+        space: str = DEFAULT_SPACE,
+        data_model: DataModel[View] | None = None,
     ):
         self._source_dir = source_dir
         self._data_set_id = data_set_id
         self._echo: Callable[[str], None] = echo or print
+        self._space = space
+        self._data_model = data_model
 
     def populate(self, client: CogniteClient):
-        ...
+        """
+        Populate the APM model with mock data included in pygen.
 
-    def populate_timeseries(self, client: CogniteClient):
-        if not (file_path := self._source_dir / "TimeSeries").exists():
-            self._echo(f"Skipping timeseries population, no data found in {file_path}")
-            return
+        Args:
+            client: Connected CogniteClient
 
-    def populate_files(self, client: CogniteClient):
-        if not (file_path := self._source_dir / "Files").exists():
-            self._echo(f"Skipping files population, no data found in {file_path}")
-            return
+        Returns:
+        """
+        self.populate_timeseries(client)
+        self.populate_files(client)
+        self.populate_nodes(client)
+        self.populate_edges(client)
 
-    def populate_nodes(self, client: CogniteClient):
-        ...
+    def populate_timeseries(self, client: CogniteClient) -> TimeSeriesList:
+        return self._populate_cdf_resource(client.time_series, TimeSeries, TimeSeriesList)
 
-    def populate_edges(self, client: CogniteClient):
-        ...
+    def populate_files(self, client: CogniteClient) -> FileMetadataList:
+        return self._populate_cdf_resource(
+            cast(CogniteCoreResourceAPI[FileMetadataList], client.files), FileMetadata, FileMetadataList
+        )
 
-    def _read_csv(self, file_path: pathlib.Path) -> tuple[list[str], list[dict[str, Any]]]:
-        raise NotImplementedError()
+    def populate_nodes(self, client: CogniteClient) -> NodeApplyResultList:
+        if not self._data_model:
+            raise ValueError("Missing data model, please pass it to the constructor")
+        all_nodes = []
+        for view in self._data_model.views:
+            properties: dict[str, MappedProperty] = {
+                name: prop for name, prop in view.properties.items() if isinstance(prop, MappedProperty)
+            }
+            if not properties:
+                continue
+            raw_nodes = self._load_resource(NodeApply, file_name=view.external_id, properties=properties)
+            # Need to split properties from general node data
+            for raw_node in raw_nodes:
+                external_id = self._pop_or_raise(raw_node, "externalId")
+                data_properties: dict[ContainerId, dict] = defaultdict(dict)
+                for name, value in raw_node.items():
+                    if prop := properties.get(name):
+                        data_properties[prop.container][name] = value
+                node = NodeApply(
+                    external_id=external_id,
+                    space=self._space,
+                    sources=[
+                        NodeOrEdgeData(source=container, properties=properties)
+                        for container, properties in data_properties.items()
+                    ],
+                )
+                all_nodes.append(node)
+
+        if not all_nodes:
+            self._echo("No nodes to create")
+            return NodeApplyResultList([])
+
+        created = client.data_modeling.instances.apply(nodes=all_nodes)
+        self._echo(f"Created {len(created.nodes)} nodes")
+        return created.nodes
+
+    @staticmethod
+    def _pop_or_raise(d: dict, camel_case: str) -> Any:
+        if camel_case in d:
+            return d.pop(camel_case)
+        elif (snake := to_snake_case(camel_case)) in d:
+            return d.pop(snake)
+        else:
+            raise ValueError(f"Missing {camel_case} in {d}")
+
+    def populate_edges(self, client: CogniteClient) -> EdgeApplyResultList:
+        if not self._data_model:
+            raise ValueError("Missing data model, please pass it to the constructor")
+        all_edges = []
+        for view in self._data_model.views:
+            for name, prop in view.properties.items():
+                if not isinstance(prop, SingleHopConnectionDefinition):
+                    continue
+                raw_edges = self._load_resource(EdgeApply, file_name=f"{view.external_id}.{name}")
+                for raw_edge in raw_edges:
+                    source_id = self._pop_or_raise(raw_edge, "sourceExternalId")
+                    target_id = self._pop_or_raise(raw_edge, "targetExternalId")
+                    edge = EdgeApply(
+                        external_id=f"{source_id}.{target_id}",
+                        space=self._space,
+                        type=prop.type,
+                        start_node=DirectRelationReference(self._space, source_id),
+                        end_node=DirectRelationReference(self._space, target_id),
+                    )
+                    all_edges.append(edge)
+        if not all_edges:
+            self._echo("No edges to create")
+            return EdgeApplyResultList([])
+        created = client.data_modeling.instances.apply(edges=all_edges)
+        self._echo(f"Created {len(created.edges)} edges")
+        return created.edges
+
+    @staticmethod
+    def _read_csv(
+        file_path: pathlib.Path,
+        resource_cls: type[T_CogniteResource] | None = None,
+        properties: dict[str, MappedProperty] | None = None,
+    ) -> list[dict[str, Any]]:
+        converter = DataTypesConverter(resource_cls=resource_cls, properties=properties)
+        with file_path.open() as file:
+            reader = csv.reader(file)
+            columns = next(reader)
+
+            rows = []
+            for raw_row in reader:
+                row = converter.convert_dtypes(dict(zip(columns, raw_row)))
+                rows.append(row)
+        return rows
+
+    def _load_resource(
+        self,
+        resource_cls: type[T_CogniteResource],
+        file_name: str | None = None,
+        properties: dict[str, MappedProperty] | None = None,
+    ) -> list[dict[str, Any]]:
+        resource_name = file_name or resource_cls.__name__
+        if not (file_path := self._source_dir / f"{resource_name}.csv").exists():
+            return []
+        return self._read_csv(file_path, resource_cls=resource_cls, properties=properties)
+
+    def _create_resource(
+        self, api: CogniteCoreResourceAPI[T_CogniteResourceList], resources: T_CogniteResourceList
+    ) -> T_CogniteResourceList:
+        if not resources:
+            return resources
+        resource_name = type(resources[0]).__name__
+        new_ids = [t.external_id for t in resources]
+        retrieved = api.retrieve_multiple(external_ids=new_ids, ignore_unknown_ids=True)
+        existing = [ts.external_id for ts in retrieved if ts]
+        if not (missing := set(new_ids) - set(existing)):
+            self._echo(f"Skipping {resource_name} creation, all {len(existing)} {resource_name} already exist")
+            return retrieved
+        if isinstance(api, FilesAPI):
+            created = []
+            for file in resources:
+                if file.external_id in missing:
+                    created.append(api.create(file))
+        else:
+            created = api.create(type(resources)([item for item in resources if item.external_id in missing]))
+        self._echo(f"Created {len(created)} {resource_name}")
+        return type(resources)(created + retrieved)
+
+    def _populate_cdf_resource(
+        self,
+        api: CogniteCoreResourceAPI[T_CogniteResourceList],
+        resource_cls: type[T_CogniteResource],
+        cls_list: type[T_CogniteResourceList],
+    ) -> T_CogniteResourceList:
+        raw_data = self._load_resource(resource_cls=resource_cls)
+        resources = cls_list._load(raw_data)
+        if not resources:
+            self._echo(f"Skipping {type(resource_cls.__name__)} population, no data found in {self._source_dir}")
+            return resources
+        return self._create_resource(api=api, resources=resources)
+
+
+class DataTypesConverter:
+    supported_timestamp_formats = ("%Y-%m-%dT%H:%M:%SZ", "%d/%m/%Y", "%Y-%m-%dT%H:%M:%S.%fZ")
+
+    def __init__(
+        self, resource_cls: type[CogniteResource] | None = None, properties: dict[str, MappedProperty] | None = None
+    ):
+        self._type_hint_by_name = self._load_type_hints(resource_cls)
+        self._property_by_name = self._load_properties(properties)
+
+    @staticmethod
+    def _load_type_hints(resource_cls: type[CogniteResource] | None = None) -> dict[str, Any]:
+        if resource_cls is None:
+            return {}
+        try:
+            return get_type_hints(resource_cls.__init__, localns={"CogniteClient": CogniteClient})
+        except TypeError:
+            return {}
+
+    @staticmethod
+    def _load_properties(
+        properties: dict[str, MappedProperty] | None = None
+    ) -> dict[str, tuple[ContainerId, data_types.PropertyType]]:
+        if properties is None:
+            return {}
+        return {name: (prop.container, prop.type) for name, prop in properties.items()}
+
+    def convert_dtypes(self, row: dict[str, Any]) -> dict[str, Any]:
+        if not self._type_hint_by_name and not self._property_by_name:
+            return row
+        for key, value in list(row.items()):
+            snake_key = to_snake_case(key)
+            if type_hint := self._type_hint_by_name.get(snake_key):
+                row[key] = self._convert_type_hint(value, type_hint)
+            elif (entry := self._property_by_name.get(key)) or (entry := self._property_by_name.get(snake_key)):
+                container, property_type = entry
+                row[key] = self._convert_property(value, property_type, container)
+        return row
+
+    @staticmethod
+    def _convert_type_hint(value: Any, outer_type_hint: Any) -> Any:
+        if get_origin(outer_type_hint) is Union:
+            type_hint, *_ = get_args(outer_type_hint)
+        else:
+            type_hint = outer_type_hint
+
+        if type_hint is bool:
+            return value.lower() == "true"
+        elif type_hint is int:
+            return int(value)
+        elif type_hint is float:
+            return float(value)
+        elif type_hint is str:
+            return str(value)
+        return value
+
+    @classmethod
+    def _convert_property(cls, value: Any, property_type: data_types.PropertyType, container_id: ContainerId) -> Any:
+        if isinstance(property_type, data_types.Boolean):
+            return value.lower() == "true"
+        elif isinstance(property_type, (data_types.Int32, data_types.Int64)):
+            return int(value)
+        elif isinstance(property_type, (data_types.Float32, data_types.Float64)):
+            return float(value)
+        elif isinstance(property_type, data_types.Text):
+            return str(value)
+        elif isinstance(property_type, data_types.Timestamp):
+            # YYYY-MM-DDTHH:MM:SS[.millis][Z|time zone]
+            for supported_format in cls.supported_timestamp_formats:
+                try:
+                    return datetime.strptime(value, supported_format).isoformat()
+                except ValueError:
+                    pass
+            raise ValueError(f"Unsupported timestamp format: {value}")
+        elif isinstance(property_type, data_types.DirectRelation):
+            return {"externalId": str(value), "space": container_id.space}
+        elif isinstance(property_type, data_types.CDFExternalIdReference):
+            return {"externalId": str(value)}
+
+        return value
+
+
+if __name__ == "__main__":
+    # Quick and dirty way to do local ad-hoc testing
+    from yaml import safe_dump, safe_load
+
+    from cognite.pygen import load_cognite_client_from_toml
+
+    c = load_cognite_client_from_toml("../../../config.toml")
+
+    local = pathlib.Path(__file__).parent / "model.yaml"
+    if not local.exists():
+        apm = SolarAPM()
+        model = apm.deploy(c)
+        local.write_text(safe_dump(model.dump(camel_case=True)))
+    else:
+        model = DataModel._load(safe_load(local.read_text()))
+
+    CSVLoader(_DATA_FOLDER, data_model=model).populate_edges(c)
