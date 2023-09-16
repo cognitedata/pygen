325f3ebc327ab16a7c8ecf86f47cfeab32521c9e
anders-albert <anders0albert@gmail.com>
2023-09-16 14:47:58 +0200

    refactor: helper

--- cognite/pygen/utils/helper.py
+++ cognite/pygen/utils/helper.py
@@ -1,3 +1,7 @@
+import contextlib
+import os
+from collections.abc import Iterator
+from pathlib import Path
 from typing import Literal
 
 from pydantic.version import VERSION as PYDANTIC_VERSION
@@ -11,3 +15,22 @@ def get_pydantic_version() -> Literal["v1", "v2"]:
         return "v2"
     else:
         raise ValueError(f"Unknown pydantic version: {PYDANTIC_VERSION}")
+
+
+@contextlib.contextmanager
+def chdir(new_dir: Path) -> Iterator[None]:
+    """
+    Change directory to new_dir and return to the original directory when exiting the context.
+
+    Args:
+        new_dir: The new directory to change to.
+
+    """
+    current_working_dir = Path.cwd()
+    os.chdir(new_dir)
+
+    try:
+        yield
+
+    finally:
+        os.chdir(current_working_dir)
