7a4014b14aba6f6db192bff33cecc4928ce8fdbd
anders-albert <anders0albert@gmail.com>
2023-09-17 18:35:51 +0200

    build: cognite-sdk in v1

--- examples-pydantic-v1/shop_pydantic_v1/client/_api_client.py
+++ examples-pydantic-v1/shop_pydantic_v1/client/_api_client.py
@@ -15,7 +15,7 @@ class ShopClient:
 
     Generated with:
         pygen = 0.17.7
-        cognite-sdk = 6.21.0
+        cognite-sdk = 6.25.1
         pydantic = 1.10.7
 
     Data Model:
--- examples-pydantic-v1/tutorial_apm_simple_pydantic_v1/client/_api_client.py
+++ examples-pydantic-v1/tutorial_apm_simple_pydantic_v1/client/_api_client.py
@@ -19,7 +19,7 @@ class ApmSimpleClient:
 
     Generated with:
         pygen = 0.17.7
-        cognite-sdk = 6.21.0
+        cognite-sdk = 6.25.1
         pydantic = 1.10.7
 
     Data Model:
