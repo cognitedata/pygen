340d6155677efd722d303c2c871e35292831fee9
anders-albert <anders0albert@gmail.com>
2023-09-24 11:22:24 +0200

    refator: regen v2 examples

--- examples/tutorial_apm_simple/client/_api/_core.py
+++ examples/tutorial_apm_simple/client/_api/_core.py
@@ -11,6 +11,8 @@ from tutorial_apm_simple.client.data_classes._core import T_TypeApplyNode, T_Typ
 
 DEFAULT_LIMIT_READ = 25
 
+INSTANCE_QUERY_LIMIT = 10_000
+
 
 class TypeAPI(Generic[T_TypeNode, T_TypeApplyNode, T_TypeNodeList]):
     def __init__(
