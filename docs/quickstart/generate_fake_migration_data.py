0c3018d39fbe3dc556ffc4ac52826f4faf35ee89
anders-albert <anders0albert@gmail.com>
2023-11-05 12:02:07 +0100

    refactor: wip on data migration

--- docs/quickstart/generate_fake_migration_data.py
+++ docs/quickstart/generate_fake_migration_data.py
@@ -8,22 +8,14 @@ from cognite.pygen import generate_sdk, load_cognite_client_from_toml
 
 def generate():
     client = load_cognite_client_from_toml()
-    generate_sdk(
-        ("APM_AppData_4", "APM_AppData_4", "7"),
-        client,
-        "apm_domain",
-        "APMClient",
-    )
+    generate_sdk(("APM_AppData_4", "APM_AppData_4", "7"), client, "apm_domain", "APMClient", overwrite=True)
 
 
-def main():
-    import sys
-    from pathlib import Path
-
-    sys.path.append(str(Path(__file__).parent))
+def create_fake_data():
+    _cwd_to_path()
 
     from apm_domain import APMClient
-    from apm_domain.data_classes import APMTemplateApply, APMTemplateItemApply
+    from apm_domain.data_classes import APMTemplateApply, APMTemplateApplyList, APMTemplateItemApply
 
     space = "sourceSpace"
     load_cognite_client_from_toml().data_modeling.spaces.apply(
@@ -63,17 +55,35 @@ def main():
                 created_by_external_id=random.choice(user_external_ids),
                 is_archived=random.choice([True, False]),
                 root_location_external_id=random.choice(location_external_ids),
+                assigned_to=random.choices(user_external_ids, k=random.randint(0, 3)),
                 status=random.choice(status),
                 title=fake.word(),
                 template_items=items,
                 updated_by_external_id=random.choice(user_external_ids),
             )
         )
+    templates = APMTemplateApplyList(templates)
 
     client = APMClient(load_cognite_client_from_toml())
     result = client.apm_template.apply(templates)
     print(len(result.nodes), len(result.edges))
 
 
+def _cwd_to_path():
+    import sys
+    from pathlib import Path
+
+    sys.path.append(str(Path(__file__).parent))
+
+
+def check_listing():
+    _cwd_to_path()
+    from apm_domain import APMClient
+
+    client = APMClient(load_cognite_client_from_toml())
+    templates = client.apm_template.list(space="sourceSpace", retrieve_edges=True, limit=-1)
+    print(len(templates), sum(len(t.template_items or []) for t in templates))
+
+
 if __name__ == "__main__":
-    main()
+    check_listing()
