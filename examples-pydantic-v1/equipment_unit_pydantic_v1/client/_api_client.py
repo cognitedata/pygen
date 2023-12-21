50b0253443377a2329244ba38a62490b51504402
Anders Albert <60234212+doctrino@users.noreply.github.com>
2023-12-21 13:01:32 +0100

    [PYG-72] Remove disclaimer (#130)
    * fix: remove wrong version
    
    * docs: Replace experimental with alpha
    
    * docs: update docs
    
    * style: fix typo

--- examples-pydantic-v1/equipment_unit_pydantic_v1/client/_api_client.py
+++ examples-pydantic-v1/equipment_unit_pydantic_v1/client/_api_client.py
@@ -16,7 +16,7 @@ class EquipmentUnitClient:
     EquipmentUnitClient
 
     Generated with:
-        pygen = 0.32.3
+        pygen = 0.32.4
         cognite-sdk = 7.5.1
         pydantic = 1.10.7
 
@@ -34,7 +34,7 @@ class EquipmentUnitClient:
         else:
             raise ValueError(f"Expected CogniteClient or ClientConfig, got {type(config_or_client)}")
         # The client name is used for aggregated logging of Pygen Usage
-        client.config.client_name = "CognitePygen:0.32.3"
+        client.config.client_name = "CognitePygen:0.32.4"
 
         view_by_write_class = {
             data_classes.EquipmentModuleApply: dm.ViewId(
