8a3e7c294dd2e89898143ac1a619b4ba738d4b0e
anders-albert <anders0albert@gmail.com>
2024-01-22 19:52:48 +0100

    refactor: regen

--- examples-pydantic-v1/omni_pydantic_v1/data_classes/_cdf_external_references_listed.py
+++ examples-pydantic-v1/omni_pydantic_v1/data_classes/_cdf_external_references_listed.py
@@ -7,6 +7,7 @@ from cognite.client.data_classes import TimeSeries
 
 from ._core import (
     DEFAULT_INSTANCE_SPACE,
+    DataRecordWrite,
     DomainModel,
     DomainModelCore,
     DomainModelApply,
@@ -45,13 +46,10 @@ class CDFExternalReferencesListed(DomainModel):
     Args:
         space: The space where the node is located.
         external_id: The external id of the cdf external references listed.
+        data_record: The data record of the cdf external references listed node.
         files: The file field.
         sequences: The sequence field.
         timeseries: The timesery field.
-        created_time: The created time of the cdf external references listed node.
-        last_updated_time: The last updated time of the cdf external references listed node.
-        deleted_time: If present, the deleted time of the cdf external references listed node.
-        version: The version of the cdf external references listed node.
     """
 
     space: str = DEFAULT_INSTANCE_SPACE
@@ -65,7 +63,7 @@ class CDFExternalReferencesListed(DomainModel):
         return CDFExternalReferencesListedApply(
             space=self.space,
             external_id=self.external_id,
-            existing_version=self.version,
+            data_record=DataRecordWrite(existing_version=self.data_record.version),
             files=self.files,
             sequences=self.sequences,
             timeseries=self.timeseries,
@@ -80,13 +78,10 @@ class CDFExternalReferencesListedApply(DomainModelApply):
     Args:
         space: The space where the node is located.
         external_id: The external id of the cdf external references listed.
+        data_record: The data record of the cdf external references listed node.
         files: The file field.
         sequences: The sequence field.
         timeseries: The timesery field.
-        existing_version: Fail the ingestion request if the cdf external references listed version is greater than or equal to this value.
-            If no existingVersion is specified, the ingestion will always overwrite any existing data for the edge (for the specified container or instance).
-            If existingVersion is set to 0, the upsert will behave as an insert, so it will fail the bulk if the item already exists.
-            If skipOnVersionConflict is set on the ingestion request, then the item will be skipped instead of failing the ingestion request.
     """
 
     space: str = DEFAULT_INSTANCE_SPACE
@@ -126,7 +121,7 @@ class CDFExternalReferencesListedApply(DomainModelApply):
             this_node = dm.NodeApply(
                 space=self.space,
                 external_id=self.external_id,
-                existing_version=self.existing_version,
+                existing_version=self.data_record.existing_version,
                 type=self.node_type,
                 sources=[
                     dm.NodeOrEdgeData(
