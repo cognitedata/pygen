f7f80aa4fe72087a673ef4298a3ad6ae3d28958e
anders-albert <anders0albert@gmail.com>
2024-01-25 11:18:53 +0100

    refactor: fix test

--- tests/test_integration/test_sdks/test_equipment_sdk/test_unit_procedure.py
+++ tests/test_integration/test_sdks/test_equipment_sdk/test_unit_procedure.py
@@ -59,7 +59,7 @@ def test_single_unit_procedure_to_pandas(unit_procedure_list: UnitProcedureList)
     procedure = unit_procedure_list[0]
 
     series = procedure.to_pandas()
-    assert len(series) == 6, "We only have the four properties + the external id and node type"
+    assert len(series) == 8, "We only have the four properties + the external id, space, node type, and data_record"
 
 
 def test_filter_start_end_time_edges(start_end_time_edges: StartEndTimeList, workorder: EquipmentUnitClient) -> None:
