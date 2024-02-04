62a1827b450606ce470204e2073ca7c81f5d6878
anders-albert <anders0albert@gmail.com>
2024-02-04 12:41:22 +0100

    tests: Added failing generation tests

---
+++ tests/test_unit/test_generator/test_generate_api_class_timeseries.py
@@ -0,0 +1,26 @@
+import difflib
+
+from cognite.client import data_modeling as dm
+
+from cognite.pygen._core.generators import MultiAPIGenerator
+from cognite.pygen._generator import CodeFormatter
+from tests.constants import OmniFiles
+
+
+def test_generate_cdf_cdf_external_references(
+    omni_multi_api_generator: MultiAPIGenerator, code_formatter: CodeFormatter
+):
+    # Arrange
+    api_generator = omni_multi_api_generator.api_by_view_id[dm.ViewId("pygen-models", "CDFExternalReferences", "1")]
+    expected = OmniFiles.cdf_external_timeseries_api.read_text()
+
+    # Act
+    _, actual = next(
+        api_generator.generate_timeseries_api_files(
+            omni_multi_api_generator.top_level_package, omni_multi_api_generator.client_name
+        )
+    )
+    actual = code_formatter.format_code(actual)
+
+    # Assert
+    assert actual == expected, "\n".join(difflib.unified_diff(expected.splitlines(), actual.splitlines()))
