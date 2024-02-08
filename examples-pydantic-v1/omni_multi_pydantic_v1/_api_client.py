715c3c00449b7e395303d4240cc7e951bbc6a544
Anders Albert <60234212+doctrino@users.noreply.github.com>
2024-02-08 08:37:51 +0100

    [PYG-115] Mock gen key error (#164)
    * fix: Handle view not in data model
    
    * build: bump
    
    * ci: update pre-commit hook

--- examples-pydantic-v1/omni_multi_pydantic_v1/_api_client.py
+++ examples-pydantic-v1/omni_multi_pydantic_v1/_api_client.py
@@ -90,7 +90,7 @@ class OmniMultiClient:
     OmniMultiClient
 
     Generated with:
-        pygen = 0.99.5
+        pygen = 0.99.6
         cognite-sdk = 7.17.1
         pydantic = 1.10.7
 
@@ -104,7 +104,7 @@ class OmniMultiClient:
         else:
             raise ValueError(f"Expected CogniteClient or ClientConfig, got {type(config_or_client)}")
         # The client name is used for aggregated logging of Pygen Usage
-        client.config.client_name = "CognitePygen:0.99.5"
+        client.config.client_name = "CognitePygen:0.99.6"
 
         self.omni_multi_a = OmniMultiAAPIs(client)
         self.omni_multi_b = OmniMultiBAPIs(client)
