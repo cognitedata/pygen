0506b4c8cb1eaeb245900e5dfc5c5c01db17f106
anders-albert <anders0albert@gmail.com>
2024-02-10 08:07:35 +0100

    refactor: Rename apply to upsert

--- examples/omni/_api/cdf_external_references.py
+++ examples/omni/_api/cdf_external_references.py
@@ -109,10 +109,11 @@ class CDFExternalReferencesAPI(NodeAPI[CDFExternalReferences, CDFExternalReferen
         """
         warnings.warn(
             "The .apply method is deprecated and will be removed in v1.0. "
-            "Please use the .apply method on the client instead. This means instead of "
-            "`my_client.cdf_external_references.apply(my_items)` please use `my_client.apply(my_items)`."
+            "Please use the .upsert method on the client instead. This means instead of "
+            "`my_client.cdf_external_references.apply(my_items)` please use `my_client.upsert(my_items)`."
             "The motivation is that all apply methods are the same, and having one apply method per API "
-            " class encourages users to create items in small batches, which is inefficient.",
+            " class encourages users to create items in small batches, which is inefficient."
+            "In addition, .upsert method is more descriptive of what the method does.",
             UserWarning,
             stacklevel=2,
         )
