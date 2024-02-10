f7c333c512cc31870cee5dc416c1601ee2f2a353
anders-albert <anders0albert@gmail.com>
2024-02-10 18:10:16 +0100

    build: bump'

--- examples-pydantic-v1/equipment_unit_pydantic_v1/client/_api_client.py
+++ examples-pydantic-v1/equipment_unit_pydantic_v1/client/_api_client.py
@@ -21,7 +21,7 @@ class EquipmentUnitClient:
     EquipmentUnitClient
 
     Generated with:
-        pygen = 0.99.6
+        pygen = 0.99.7
         cognite-sdk = 7.17.1
         pydantic = 1.10.7
 
@@ -39,7 +39,7 @@ class EquipmentUnitClient:
         else:
             raise ValueError(f"Expected CogniteClient or ClientConfig, got {type(config_or_client)}")
         # The client name is used for aggregated logging of Pygen Usage
-        client.config.client_name = "CognitePygen:0.99.6"
+        client.config.client_name = "CognitePygen:0.99.7"
 
         view_by_read_class = {
             data_classes.EquipmentModule: dm.ViewId("IntegrationTestsImmutable", "EquipmentModule", "b1cd4bf14a7a33"),
--- examples-pydantic-v1/omni_multi_pydantic_v1/_api_client.py
+++ examples-pydantic-v1/omni_multi_pydantic_v1/_api_client.py
@@ -91,7 +91,7 @@ class OmniMultiClient:
     OmniMultiClient
 
     Generated with:
-        pygen = 0.99.6
+        pygen = 0.99.7
         cognite-sdk = 7.17.1
         pydantic = 1.10.7
 
@@ -105,7 +105,7 @@ class OmniMultiClient:
         else:
             raise ValueError(f"Expected CogniteClient or ClientConfig, got {type(config_or_client)}")
         # The client name is used for aggregated logging of Pygen Usage
-        client.config.client_name = "CognitePygen:0.99.6"
+        client.config.client_name = "CognitePygen:0.99.7"
 
         self.omni_multi_a = OmniMultiAAPIs(client)
         self.omni_multi_b = OmniMultiBAPIs(client)
