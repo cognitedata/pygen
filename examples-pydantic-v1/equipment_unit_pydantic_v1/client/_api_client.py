3117cbb9d66eec17083f1884a7f837e2b7323291
anders-albert <anders0albert@gmail.com>
2024-02-23 10:46:25 +0100

    build; bump

--- examples-pydantic-v1/equipment_unit_pydantic_v1/client/_api_client.py
+++ examples-pydantic-v1/equipment_unit_pydantic_v1/client/_api_client.py
@@ -21,7 +21,7 @@ class EquipmentUnitClient:
     EquipmentUnitClient
 
     Generated with:
-        pygen = 0.99.10
+        pygen = 0.99.11
         cognite-sdk = 7.17.1
         pydantic = 1.10.7
 
@@ -39,7 +39,7 @@ class EquipmentUnitClient:
         else:
             raise ValueError(f"Expected CogniteClient or ClientConfig, got {type(config_or_client)}")
         # The client name is used for aggregated logging of Pygen Usage
-        client.config.client_name = "CognitePygen:0.99.10"
+        client.config.client_name = "CognitePygen:0.99.11"
 
         view_by_read_class = {
             data_classes.EquipmentModule: dm.ViewId("IntegrationTestsImmutable", "EquipmentModule", "b1cd4bf14a7a33"),
