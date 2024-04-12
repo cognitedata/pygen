b496827dff22802d723df6716a059dc7a67dfc96
anders-albert <anders0albert@gmail.com>
2024-04-12 14:41:22 +0200

    tests: Added failing test

--- tests/test_integration/test_graphql_query.py
+++ tests/test_integration/test_graphql_query.py
@@ -3,9 +3,13 @@ from __future__ import annotations
 from tests.constants import IS_PYDANTIC_V2
 
 if IS_PYDANTIC_V2:
+    from omni import OmniClient
+    from omni import data_classes as odc
     from windmill import WindmillClient
     from windmill import data_classes as wdc
 else:
+    from omni_multi_pydantic_v1 import OmniClient
+    from omni_multi_pydantic_v1 import data_classes as odc
     from windmill_pydantic_v1 import WindmillClient
     from windmill_pydantic_v1 import data_classes as wdc
 
@@ -24,3 +28,32 @@ def test_graphql_query(wind_client: WindmillClient) -> None:
     assert len(result) == 1
     assert isinstance(result[0], wdc.BladeGraphQL)
     assert result[0].name
+
+
+def test_query_cdf_external(omni_client: OmniClient) -> None:
+    query = """{
+  listCDFExternalReferencesListed{
+    items{
+       __typename
+      timeseries {
+        externalId
+        name
+      }
+      externalId
+      files{
+        externalId
+        name
+      }
+      sequences{
+        externalId
+        name
+
+      }
+    }
+  }
+}
+"""
+    result = omni_client.graphql_query(query)
+
+    assert len(result) > 0
+    assert isinstance(result[0], odc.CDFExternalReferencesGraphQL)
