6b608239f6aefca46a9a5dd6089111cbb645336c
anders-albert <anders0albert@gmail.com>
2024-05-02 09:34:01 +0200

    build; bump and changelog

--- examples/windmill/_api_client.py
+++ examples/windmill/_api_client.py
@@ -29,7 +29,7 @@ class WindmillClient:
     WindmillClient
 
     Generated with:
-        pygen = 0.99.20
+        pygen = 0.99.21
         cognite-sdk = 7.37.1
         pydantic = 2.7.0
 
@@ -47,7 +47,7 @@ class WindmillClient:
         else:
             raise ValueError(f"Expected CogniteClient or ClientConfig, got {type(config_or_client)}")
         # The client name is used for aggregated logging of Pygen Usage
-        client.config.client_name = "CognitePygen:0.99.20"
+        client.config.client_name = "CognitePygen:0.99.21"
 
         view_by_read_class = {
             data_classes.Blade: dm.ViewId("power-models", "Blade", "1"),
