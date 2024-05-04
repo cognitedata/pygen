3490349b9e05f40a0bb060ed2c5488b3cedbb9f9
anders-albert <anders0albert@gmail.com>
2024-05-04 09:18:51 +0200

    refactor: regen v1 SDKs

---
+++ examples-pydantic-v1/omni_pydantic_v1/_api/connection_item_f_query.py
@@ -0,0 +1,74 @@
+from __future__ import annotations
+
+import datetime
+from typing import TYPE_CHECKING
+
+from cognite.client import data_modeling as dm, CogniteClient
+
+from omni_pydantic_v1.data_classes import (
+    DomainModelCore,
+    ConnectionItemF,
+    ConnectionItemD,
+)
+from ._core import DEFAULT_QUERY_LIMIT, QueryBuilder, QueryStep, QueryAPI, T_DomainModelList, _create_edge_filter
+
+
+class ConnectionItemFQueryAPI(QueryAPI[T_DomainModelList]):
+    def __init__(
+        self,
+        client: CogniteClient,
+        builder: QueryBuilder[T_DomainModelList],
+        view_by_read_class: dict[type[DomainModelCore], dm.ViewId],
+        filter_: dm.filters.Filter | None = None,
+        limit: int = DEFAULT_QUERY_LIMIT,
+    ):
+        super().__init__(client, builder, view_by_read_class)
+
+        self._builder.append(
+            QueryStep(
+                name=self._builder.next_name("connection_item_f"),
+                expression=dm.query.NodeResultSetExpression(
+                    from_=self._builder[-1].name if self._builder else None,
+                    filter=filter_,
+                ),
+                select=dm.query.Select([dm.query.SourceSelector(self._view_by_read_class[ConnectionItemF], ["*"])]),
+                result_cls=ConnectionItemF,
+                max_retrieve_limit=limit,
+            )
+        )
+
+    def query(
+        self,
+        retrieve_direct_list: bool = False,
+    ) -> T_DomainModelList:
+        """Execute query and return the result.
+
+        Args:
+            retrieve_direct_list: Whether to retrieve the direct list for each connection item f or not.
+
+        Returns:
+            The list of the source nodes of the query.
+
+        """
+        from_ = self._builder[-1].name
+        if retrieve_direct_list:
+            self._query_append_direct_list(from_)
+        return self._query()
+
+    def _query_append_direct_list(self, from_: str) -> None:
+        view_id = self._view_by_read_class[ConnectionItemD]
+        self._builder.append(
+            QueryStep(
+                name=self._builder.next_name("direct_list"),
+                expression=dm.query.NodeResultSetExpression(
+                    filter=dm.filters.HasData(views=[view_id]),
+                    from_=from_,
+                    through=self._view_by_read_class[ConnectionItemF].as_property_ref("directList"),
+                    direction="outwards",
+                ),
+                select=dm.query.Select([dm.query.SourceSelector(view_id, ["*"])]),
+                max_retrieve_limit=-1,
+                result_cls=ConnectionItemD,
+                is_single_direct_relation=True,
+            ),
+        )
