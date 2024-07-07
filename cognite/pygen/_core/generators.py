a9e4decfe904b68130d47488cd75b112efeebbd6
anders-albert <anders0albert@gmail.com>
2024-07-07 21:23:52 +0200

    refactor: last fix

--- cognite/pygen/_core/generators.py
+++ cognite/pygen/_core/generators.py
@@ -536,7 +536,8 @@ class APIGenerator:
             raise ValueError(f"Unknown data class {type(self.data_class)}")
 
         def create_start_node_set(group: list[EdgeAPIClass]) -> str:
-            return ", ".join([g.start_class.write_name for g in group])
+            joined = ", ".join([g.start_class.write_name for g in group])
+            return f"{{{joined}}}"
 
         if is_pydantic_v2 and self.data_class.has_any_field_model_prefix:
             names = ", ".join(field.name for field in self.data_class.fields if field.name.startswith("name"))
