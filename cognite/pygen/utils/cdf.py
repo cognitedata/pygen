0dacc7822d41fdf9c9dbd1088cc95d3c93628487
anders-albert <anders0albert@gmail.com>
2024-07-15 19:50:56 +0200

    refactor; regen

--- cognite/pygen/utils/cdf.py
+++ cognite/pygen/utils/cdf.py
@@ -181,7 +181,7 @@ class CSVLoader:
         Returns:
             A list of TimeSeries created in CDF.
         """
-        return self._populate_cdf_resource(client.time_series, TimeSeries, TimeSeriesList)
+        return self._populate_cdf_resource(client.time_series, TimeSeries, TimeSeriesList)  # type: ignore[arg-type]
 
     def populate_files(self, client: CogniteClient) -> FileMetadataList:
         """
@@ -319,7 +319,7 @@ class CSVLoader:
             client: Connected CogniteClient
 
         """
-        self._clean_cdf_resource(client.time_series, TimeSeries)
+        self._clean_cdf_resource(client.time_series, TimeSeries) # type: ignore[arg-type]
 
     def clean_files(self, client: CogniteClient) -> None:
         """
