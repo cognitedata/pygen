5e89e6c47dbc4187e47c819ce42f002a4030b107
anders-albert <anders0albert@gmail.com>
2024-08-14 16:35:39 +0200

    refactor; new core

--- examples/scenario_instance/client/data_classes/_core.py
+++ examples/scenario_instance/client/data_classes/_core.py
@@ -48,6 +48,20 @@ else:
     from typing_extensions import Self
 
 
+DEFAULT_QUERY_LIMIT = 5
+INSTANCE_QUERY_LIMIT = 1_000
+# This is the actual limit of the API, we typically set it to a lower value to avoid hitting the limit.
+ACTUAL_INSTANCE_QUERY_LIMIT = 10_000
+DEFAULT_INSTANCE_SPACE = "IntegrationTestsImmutable"
+
+
+class _NotSetSentinel:
+    """This is a special class that indicates that a value has not been set.
+    It is used when we need to distinguish between not set and None."""
+
+    ...
+
+
 TimeSeries = Annotated[
     CogniteTimeSeries,
     PlainSerializer(
@@ -89,20 +103,6 @@ class TimeSeriesGraphQL(BaseModel, arbitrary_types_allowed=True, populate_by_nam
                 data["datapoints"] = datapoints["items"]
                 data["data"] = Datapoints.load(data)
         return data
-
-
-DEFAULT_QUERY_LIMIT = 5
-INSTANCE_QUERY_LIMIT = 1_000
-# This is the actual limit of the API, we typically set it to a lower value to avoid hitting the limit.
-ACTUAL_INSTANCE_QUERY_LIMIT = 10_000
-DEFAULT_INSTANCE_SPACE = "IntegrationTestsImmutable"
-
-
-class _NotSetSentinel:
-    """This is a special class that indicates that a value has not been set.
-    It is used when we need to distinguish between not set and None."""
-
-    ...
 
 
 @dataclass
