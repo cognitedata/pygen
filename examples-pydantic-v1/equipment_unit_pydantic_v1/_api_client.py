ad622c7e8713e3163f4dd4ba5ec5e427c973d922
anders-albert <anders0albert@gmail.com>
2024-09-06 13:14:11 +0200

    build: bump

--- examples-pydantic-v1/equipment_unit_pydantic_v1/_api_client.py
+++ examples-pydantic-v1/equipment_unit_pydantic_v1/_api_client.py
@@ -23,7 +23,7 @@ class EquipmentUnitClient:
     EquipmentUnitClient
 
     Generated with:
-        pygen = 0.99.33
+        pygen = 0.99.34
         cognite-sdk = 7.54.17
         pydantic = 1.10.7
 
@@ -41,7 +41,7 @@ class EquipmentUnitClient:
         else:
             raise ValueError(f"Expected CogniteClient or ClientConfig, got {type(config_or_client)}")
         # The client name is used for aggregated logging of Pygen Usage
-        client.config.client_name = "CognitePygen:0.99.33"
+        client.config.client_name = "CognitePygen:0.99.34"
 
         self._client = client
 
--- examples-pydantic-v1/scenario_instance_pydantic_v1/client/_api_client.py
+++ examples-pydantic-v1/scenario_instance_pydantic_v1/client/_api_client.py
@@ -21,7 +21,7 @@ class ScenarioInstanceClient:
     ScenarioInstanceClient
 
     Generated with:
-        pygen = 0.99.33
+        pygen = 0.99.34
         cognite-sdk = 7.54.17
         pydantic = 1.10.7
 
@@ -39,7 +39,7 @@ class ScenarioInstanceClient:
         else:
             raise ValueError(f"Expected CogniteClient or ClientConfig, got {type(config_or_client)}")
         # The client name is used for aggregated logging of Pygen Usage
-        client.config.client_name = "CognitePygen:0.99.33"
+        client.config.client_name = "CognitePygen:0.99.34"
 
         self._client = client
 
