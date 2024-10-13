2e6995bf843b79e08de6bfa187063afc5882374c
anders-albert <anders0albert@gmail.com>
2024-10-13 20:01:17 +0200

    tests: added back more tests

--- tests/test_integration/test_upsert.py
+++ tests/test_integration/test_upsert.py
@@ -7,7 +7,6 @@ from omni import OmniClient
 from omni import data_classes as dc
 
 
-@pytest.mark.skip("Unstable endpoints")
 def test_node_without_properties(omni_client: OmniClient, cognite_client: CogniteClient) -> None:
     # Arrange
     test_name = "integration_test:NodeWithoutProperties"
@@ -93,7 +92,6 @@ def test_upsert_multiple_requests(omni_client: OmniClient, cognite_client: Cogni
         cognite_client.data_modeling.instances.delete(resources.nodes.as_ids(), resources.edges.as_ids())
 
 
-@pytest.mark.skip("Unstable endpoints")
 def test_upsert_recursive(omni_client: OmniClient, cognite_client: CogniteClient) -> None:
     # Arrange
     test_name = "integration_test:ApplyRecursive"
@@ -173,7 +171,6 @@ def primitive_nullable_node(omni_client: OmniClient, cognite_client: CogniteClie
         cognite_client.data_modeling.instances.delete(nodes=node.as_tuple_id())
 
 
-@pytest.mark.skip("Unstable endpoints")
 def test_update_to_null(
     omni_client: OmniClient, cognite_client: CogniteClient, primitive_nullable_node: dc.PrimitiveNullableApply
 ) -> None:
@@ -203,7 +200,6 @@ def test_update_to_null(
     assert retrieved.json_ is None
 
 
-@pytest.mark.skip("Unstable endpoints")
 def test_set_empty_string(
     omni_client: OmniClient, cognite_client: CogniteClient, primitive_nullable_node: dc.PrimitiveNullableApply
 ) -> None:
