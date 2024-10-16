a583a8adaf64546d69e0fb5c2f61b7e99839a25d
anders-albert <anders0albert@gmail.com>
2024-10-16 17:30:50 +0200

    build; bump

--- examples/equipment_unit/_api_client.py
+++ examples/equipment_unit/_api_client.py
@@ -23,7 +23,7 @@ class EquipmentUnitClient:
     EquipmentUnitClient
 
     Generated with:
-        pygen = 0.99.37
+        pygen = 0.99.38
         cognite-sdk = 7.63.3
         pydantic = 2.9.2
 
@@ -41,7 +41,7 @@ class EquipmentUnitClient:
         else:
             raise ValueError(f"Expected CogniteClient or ClientConfig, got {type(config_or_client)}")
         # The client name is used for aggregated logging of Pygen Usage
-        client.config.client_name = "CognitePygen:0.99.37"
+        client.config.client_name = "CognitePygen:0.99.38"
 
         self._client = client
 
