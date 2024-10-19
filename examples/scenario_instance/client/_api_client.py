bbf4f0e31a63a1e1336e70a72bfe29d4bbfd0686
anders-albert <anders0albert@gmail.com>
2024-10-19 23:22:56 +0200

    build; bump

--- examples/scenario_instance/client/_api_client.py
+++ examples/scenario_instance/client/_api_client.py
@@ -21,7 +21,7 @@ class ScenarioInstanceClient:
     ScenarioInstanceClient
 
     Generated with:
-        pygen = 0.99.38
+        pygen = 0.99.39
         cognite-sdk = 7.63.6
         pydantic = 2.9.2
 
@@ -39,7 +39,7 @@ class ScenarioInstanceClient:
         else:
             raise ValueError(f"Expected CogniteClient or ClientConfig, got {type(config_or_client)}")
         # The client name is used for aggregated logging of Pygen Usage
-        client.config.client_name = "CognitePygen:0.99.38"
+        client.config.client_name = "CognitePygen:0.99.39"
 
         self._client = client
 
