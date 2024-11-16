a7f488521d1d3ef52a540850f1f03b0a07bd3aeb
anders-albert <anders0albert@gmail.com>
2024-11-16 22:35:27 +0100

    tests: seplace equipment tests

--- tests/test_integration/test_sdks/test_equipment_sdk/conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from equipment_unit import EquipmentUnitClient
-
-
-@pytest.fixture()
-def workorder(client_config) -> EquipmentUnitClient:
-    return EquipmentUnitClient.azure_project(**client_config)
