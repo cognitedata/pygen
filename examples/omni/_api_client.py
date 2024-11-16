f98a8488094d80f41f545b2af1877e95c60261b8
anders-albert <anders0albert@gmail.com>
2024-11-16 10:25:50 +0100

    build: update

--- examples/omni/_api_client.py
+++ examples/omni/_api_client.py
@@ -42,7 +42,7 @@ class OmniClient:
 
     Generated with:
         pygen = 0.99.49
-        cognite-sdk = 7.64.12
+        cognite-sdk = 7.66.0
         pydantic = 2.9.2
 
     Data Model:
--- examples/omni_sub/_api_client.py
+++ examples/omni_sub/_api_client.py
@@ -24,7 +24,7 @@ class OmniSubClient:
 
     Generated with:
         pygen = 0.99.49
-        cognite-sdk = 7.64.12
+        cognite-sdk = 7.66.0
         pydantic = 2.9.2
 
     Data Model:
