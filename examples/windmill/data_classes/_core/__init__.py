dbe8bee83cd345b5aee297028b81ac29bb145811
anders-albert <anders0albert@gmail.com>
2024-11-16 19:50:25 +0100

    refactor: deleted windmill

--- examples/windmill/data_classes/_core/__init__.py
+++
@@ -1,5 +0,0 @@
-from windmill.data_classes._core.constants import *  # noqa
-from windmill.data_classes._core.base import *  # noqa
-from windmill.data_classes._core.cdf_external import *  # noqa
-from windmill.data_classes._core.helpers import *  # noqa
-from windmill.data_classes._core.query import *  # noqa
