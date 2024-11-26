e28cf1ff4f09c732fdb310f9af03fd8489ffc2cc
anders-albert <anders0albert@gmail.com>
2024-11-26 16:32:24 +0100

    build; bump

--- examples/cognite_core/_api_client.py
+++ examples/cognite_core/_api_client.py
@@ -50,7 +50,7 @@ class CogniteCoreClient:
     CogniteCoreClient
 
     Generated with:
-        pygen = 0.99.53
+        pygen = 0.99.54
         cognite-sdk = 7.66.0
         pydantic = 2.9.2
 
@@ -68,7 +68,7 @@ class CogniteCoreClient:
         else:
             raise ValueError(f"Expected CogniteClient or ClientConfig, got {type(config_or_client)}")
         # The client name is used for aggregated logging of Pygen Usage
-        client.config.client_name = "CognitePygen:0.99.53"
+        client.config.client_name = "CognitePygen:0.99.54"
 
         self._client = client
 
