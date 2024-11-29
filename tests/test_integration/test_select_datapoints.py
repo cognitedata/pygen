3feae0c646184df919e552d5d1dc3e29019d2e97
anders-albert <anders0albert@gmail.com>
2024-11-29 12:46:24 +0100

    tests: added failing test

--- tests/test_integration/test_select_datapoints.py
+++ tests/test_integration/test_select_datapoints.py
@@ -1,3 +1,4 @@
+from omni import OmniClient
 from wind_turbine import WindTurbineClient
 
 
@@ -9,6 +10,16 @@ def test_retrieve_dataframe(turbine_client: WindTurbineClient) -> None:
             limit=10,
             timeseries_limit=100,
         )
+    )
+
+    assert not df.empty
+    assert len(df) == 10
+
+
+def test_retrieve_dataframe_classic(omni_client: OmniClient) -> None:
+    df = omni_client.cdf_external_references.select().timeseries.data.retrieve_dataframe(
+        limit=10,
+        timeseries_limit=100,
     )
 
     assert not df.empty
