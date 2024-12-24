f2eda5b5e7fa3eab9348be3c6459bf4c7db61f56
anders-albert <anders0albert@gmail.com>
2024-12-24 16:48:34 +0100

    style: happier mypy

--- examples/omni/data_classes/_core/constants.py
+++ examples/omni/data_classes/_core/constants.py
@@ -1,9 +1,2 @@
 DEFAULT_QUERY_LIMIT = 5
 DEFAULT_INSTANCE_SPACE = "sp_omni_instances"
-
-
-class NotSetSentinel:
-    """This is a special class that indicates that a value has not been set.
-    It is used when we need to distinguish between not set and None."""
-
-    ...
