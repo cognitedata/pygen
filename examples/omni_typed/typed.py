fa15b64730623dded1b5db04cf14b2e287cb5088
anders-albert <anders0albert@gmail.com>
2024-12-26 09:54:04 +0100

    refactor; experiment

--- examples/omni_typed/typed.py
+++ examples/omni_typed/typed.py
@@ -12,6 +12,7 @@ from cognite.client.data_classes.data_modeling.instances import (
 
 
 class _CDFExternalReferencesListedProperties:
+
     @classmethod
     def get_source(cls) -> ViewId:
         return ViewId("sp_pygen_models", "CDFExternalReferencesListed", "1")
