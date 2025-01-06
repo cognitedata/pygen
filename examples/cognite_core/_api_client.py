4119fe0041df9fbfcc333143817e49716bf634fc
Morten Grønbech <morten.gronbech@cognite.com>
2025-01-06 14:57:28 +0100

    Regen docs with new version number

--- examples/cognite_core/_api_client.py
+++ examples/cognite_core/_api_client.py
@@ -52,7 +52,7 @@ class CogniteCoreClient:
     CogniteCoreClient
 
     Generated with:
-        pygen = 0.99.60
+        pygen = 0.99.61
         cognite-sdk = 7.70.7
         pydantic = 2.10.4
 
@@ -70,7 +70,7 @@ class CogniteCoreClient:
         else:
             raise ValueError(f"Expected CogniteClient or ClientConfig, got {type(config_or_client)}")
         # The client name is used for aggregated logging of Pygen Usage
-        client.config.client_name = "CognitePygen:0.99.60"
+        client.config.client_name = "CognitePygen:0.99.61"
 
         self._client = client
 
