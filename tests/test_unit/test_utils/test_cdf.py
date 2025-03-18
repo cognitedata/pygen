482ec8d8b298e64d8375b85f53ee5161323bf640
anders-albert <anders0albert@gmail.com>
2025-03-17 19:37:26 -0500

    tests: failing tests

--- tests/test_unit/test_utils/test_cdf.py
+++ tests/test_unit/test_utils/test_cdf.py
@@ -3,7 +3,8 @@ from __future__ import annotations
 import pytest
 from cognite.client import data_modeling as dm
 
-from cognite.pygen.utils.cdf import _find_first_node_type, _unpack_filter
+from cognite.pygen.utils.cdf import _find_first_node_type, _reduce_model, _unpack_filter
+from tests.constants import CORE_SDK
 
 
 @pytest.mark.parametrize(
@@ -88,3 +89,37 @@ def test_find_node_type(filter_: dm.Filter | None, expected: dm.DirectRelationRe
 
     # Assert
     assert actual == expected
+
+
+def test_reduce_model() -> None:
+    core = CORE_SDK.load_data_model()
+    view_by_external_id = {view.external_id: view for view in core.views}
+    exclude_views: set[str | dm.ViewId] = {
+        view.external_id for view in core.views if view.external_id not in {"CogniteAsset", "CogniteActivity"}
+    }
+
+    reduced_model = _reduce_model(core, exclude_views=exclude_views)
+
+    actual_by_external_id = {view.external_id: view for view in reduced_model.views}
+    assert set(actual_by_external_id.keys()) == {"CogniteAsset", "CogniteActivity"}
+    describable_props = set(view_by_external_id["CogniteDescribable"].properties.keys())
+    sourceable_props = set(view_by_external_id["CogniteSourceable"].properties.keys()) - {"source"}
+    assert set(actual_by_external_id["CogniteAsset"].properties.keys()) == (
+        describable_props
+        | sourceable_props
+        | {
+            "activities",
+            "parent",
+            "path",
+            "pathLastUpdatedTime",
+            "root",
+            "children",
+        }
+    )
+    assert actual_by_external_id["CogniteActivity"].properties.keys() == describable_props | sourceable_props | {
+        "assets",
+        "endTime",
+        "startTime",
+        "scheduledStartTime",
+        "scheduledEndTime",
+    }
