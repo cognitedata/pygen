fa82a71e49e617567d7dcbce6419e0fec87b3e93
anders-albert <anders0albert@gmail.com>
2025-03-29 16:42:50 +0100

    tests: updated test

--- tests/test_integration/test_query_builder.py
+++ tests/test_integration/test_query_builder.py
@@ -3,7 +3,7 @@ from cognite.client import CogniteClient
 from cognite.client import data_modeling as dm
 from cognite.client.data_classes import filters
 from omni import data_classes as dc
-from omni.data_classes._core import QueryBuilder, QueryStep, QueryUnpacker, ViewPropertyId
+from omni.data_classes._core import QueryBuilder, QueryBuildStep, QueryUnpacker, ViewPropertyId
 
 
 class TestQueryBuilder:
@@ -16,7 +16,7 @@ class TestQueryBuilder:
 
         builder = QueryBuilder()
         builder.append(
-            QueryStep(
+            QueryBuildStep(
                 builder.create_name(None),
                 dm.query.NodeResultSetExpression(
                     filter=filters.HasData(views=[item_e]),
@@ -28,7 +28,7 @@ class TestQueryBuilder:
         from_ = builder.get_from()
 
         builder.append(
-            QueryStep(
+            QueryBuildStep(
                 builder.create_name(from_),
                 dm.query.NodeResultSetExpression(
                     from_=from_,
@@ -41,7 +41,7 @@ class TestQueryBuilder:
             )
         )
         builder.append(
-            QueryStep(
+            QueryBuildStep(
                 builder.create_name(from_),
                 dm.query.NodeResultSetExpression(
                     from_=from_,
@@ -55,19 +55,21 @@ class TestQueryBuilder:
             )
         )
         # Act
-        builder.execute_query(cognite_client)
-        unpacked = QueryUnpacker(builder).unpack()
+        executor = builder.build()
+        results = executor.execute_query(cognite_client)
+        unpacked = QueryUnpacker(results).unpack()
         result = dc.ConnectionItemEList([dc.ConnectionItemE.model_validate(item) for item in unpacked])
 
         # Assert
+        status = list(executor._status_by_name.values())
         assert isinstance(result, dc.ConnectionItemEList)
-        assert builder[0].total_retrieved == len(result) > 0
+        assert status[0].total_retrieved == len(result) > 0
 
         actual_direct_set = sum(1 for item in result if item.direct_reverse_single)
-        assert builder[1].total_retrieved == actual_direct_set > 0
+        assert status[1].total_retrieved == actual_direct_set > 0
 
         actual_direct_multi_set = sum(len(item.direct_reverse_multi or []) for item in result)
-        assert 0 < builder[2].total_retrieved <= actual_direct_multi_set
+        assert 0 < status[2].total_retrieved <= actual_direct_multi_set
 
     def test_query_with_edge_and_direct_relation(
         self, cognite_client: CogniteClient, omni_views: dict[str, dm.View]
@@ -78,7 +80,7 @@ class TestQueryBuilder:
         builder = QueryBuilder()
 
         builder.append(
-            QueryStep(
+            QueryBuildStep(
                 builder.create_name(None),
                 dm.query.NodeResultSetExpression(
                     filter=filters.HasData(views=[item_a]),
@@ -89,7 +91,7 @@ class TestQueryBuilder:
         )
         from_a = builder.get_from()
         builder.append(
-            QueryStep(
+            QueryBuildStep(
                 builder.create_name(from_a),
                 dm.query.NodeResultSetExpression(
                     from_=from_a,
@@ -104,7 +106,7 @@ class TestQueryBuilder:
         )
         edge_name = builder.create_name(from_a)
         builder.append(
-            QueryStep(
+            QueryBuildStep(
                 edge_name,
                 dm.query.EdgeResultSetExpression(
                     from_=from_a,
@@ -115,7 +117,7 @@ class TestQueryBuilder:
             )
         )
         builder.append(
-            QueryStep(
+            QueryBuildStep(
                 builder.create_name(edge_name),
                 dm.query.NodeResultSetExpression(
                     from_=edge_name,
@@ -126,21 +128,23 @@ class TestQueryBuilder:
         )
 
         # Act
-        builder.execute_query(cognite_client)
-        unpacked = QueryUnpacker(builder).unpack()
+        executor = builder.build()
+        results = executor.execute_query(cognite_client)
+        unpacked = QueryUnpacker(results).unpack()
         result = dc.ConnectionItemAList([dc.ConnectionItemA.model_validate(item) for item in unpacked])
 
         # Assert
+        status = list(executor._status_by_name.values())
         assert isinstance(result, dc.ConnectionItemAList)
-        assert builder[0].total_retrieved > 0
-        assert builder[0].total_retrieved == len(result)
+        assert status[0].total_retrieved > 0
+        assert status[0].total_retrieved == len(result)
 
         actual_direct_set = sum(1 for item in result if item.other_direct)
         # The other direct can be the same for multiple items
-        assert 0 < builder[1].total_retrieved <= actual_direct_set
+        assert 0 < status[1].total_retrieved <= actual_direct_set
 
         actual_edge_set = sum(len(item.outwards or []) for item in result)
-        assert builder[2].total_retrieved == actual_edge_set > 0
+        assert status[2].total_retrieved == actual_edge_set > 0
 
         unique_destination_set = set(
             destination.as_id()
@@ -148,7 +152,7 @@ class TestQueryBuilder:
             for destination in item.outwards or []
             if isinstance(destination, dc.ConnectionItemB)
         )
-        assert builder[3].total_retrieved == len(unique_destination_set) > 0
+        assert status[3].total_retrieved == len(unique_destination_set) > 0
 
     @pytest.mark.skip(reason="Missing test data")
     def test_query_across_edge_with_properties(
@@ -157,7 +161,7 @@ class TestQueryBuilder:
         item_f = omni_views["ConnectionItemF"].as_id()
         builder = QueryBuilder()
         builder.append(
-            QueryStep(
+            QueryBuildStep(
                 builder.create_name(None),
                 dm.query.NodeResultSetExpression(
                     filter=filters.HasData(views=[item_f]),
@@ -169,7 +173,7 @@ class TestQueryBuilder:
         from_f = builder.get_from()
         edge_view = omni_views["ConnectionEdgeA"].as_id()
         builder.append(
-            QueryStep(
+            QueryBuildStep(
                 builder.create_name(from_f),
                 dm.query.EdgeResultSetExpression(
                     from_=from_f,
@@ -184,7 +188,7 @@ class TestQueryBuilder:
         item_g = omni_views["ConnectionItemG"].as_id()
         from_edge = builder.get_from()
         builder.append(
-            QueryStep(
+            QueryBuildStep(
                 builder.create_name(from_edge),
                 dm.query.NodeResultSetExpression(
                     from_=from_edge,
@@ -195,17 +199,19 @@ class TestQueryBuilder:
             )
         )
 
-        builder.execute_query(cognite_client)
-        unpacked = QueryUnpacker(builder).unpack()
+        executor = builder.build()
+        results = executor.execute_query(cognite_client)
+        unpacked = QueryUnpacker(results).unpack()
         result = dc.ConnectionItemFList([dc.ConnectionItemF.model_validate(item) for item in unpacked])
 
+        status = list(executor._status_by_name.values())
         assert isinstance(result, dc.ConnectionItemFList)
-        assert builder[0].total_retrieved == len(result) > 0
+        assert status[0].total_retrieved == len(result) > 0
 
         actual_edge_set = sum(len(item.outwards_multi or []) for item in result)
-        assert builder[1].total_retrieved == actual_edge_set > 0
+        assert status[1].total_retrieved == actual_edge_set > 0
 
         actual_node_set = sum(
             1 for item in result for edge in item.outwards_multi or [] if isinstance(edge.end_node, dc.ConnectionItemG)
         )
-        assert builder[2].total_retrieved == actual_node_set > 0
+        assert status[2].total_retrieved == actual_node_set > 0
