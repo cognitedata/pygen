734a807b0aacee269e3d9a4c5b15d3b5ce749a55
anders-albert <anders0albert@gmail.com>
2025-04-17 14:29:51 +0200

    tests: happy path

--- tests/test_integration/test_iteration.py
+++ tests/test_integration/test_iteration.py
@@ -18,3 +18,19 @@ class TestIteration:
             retrieved.extend(item)
 
         assert len(retrieved) == 3
+
+    def test_iterate_stop_resume_iteration(self, omni_client: OmniClient) -> None:
+        retrieved = dc.ConnectionItemAList()
+        cursors: dict[str, str | None] | None = None
+        for item in omni_client.connection_item_a.iterate(chunk_size=2):
+            assert len(item) <= 2
+            retrieved.extend(item)
+            cursors = item.cursors
+            break
+
+        for item in omni_client.connection_item_a.iterate(chunk_size=2, limit=3, cursors=cursors):
+            assert len(item) <= 2
+            retrieved.extend(item)
+
+        assert len(retrieved) == 3 + 2  # 2 from first iteration, 3 from the second iteration.
+        assert len(set(retrieved.as_node_ids())) == len(retrieved)
