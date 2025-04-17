1bce0ede2df6ad3065faa64d28e7d5aace287e99
anders-albert <anders0albert@gmail.com>
2025-04-17 14:35:15 +0200

    tests: added test for using same cursors twice

--- tests/test_integration/test_iteration.py
+++ tests/test_integration/test_iteration.py
@@ -1,3 +1,4 @@
+import pytest
 from omni import OmniClient
 from omni import data_classes as dc
 
@@ -34,3 +35,26 @@ class TestIteration:
 
         assert len(retrieved) == 3 + 2  # 2 from first iteration, 3 from the second iteration.
         assert len(set(retrieved.as_node_ids())) == len(retrieved)
+
+    def test_iterate_stop_resume_twice_same_cursors_raise_value_error(self, omni_client: OmniClient) -> None:
+        retrieved = dc.ConnectionItemAList()
+        cursors: dict[str, str | None] | None = None
+        for item in omni_client.connection_item_a.iterate(chunk_size=2):
+            assert len(item) <= 2
+            retrieved.extend(item)
+            cursors = item.cursors
+            break
+
+        for item in omni_client.connection_item_a.iterate(chunk_size=2, limit=3, cursors=cursors):
+            assert len(item) <= 2
+            retrieved.extend(item)
+            break
+
+        with pytest.raises(ValueError) as e:
+            for item in omni_client.connection_item_a.iterate(chunk_size=2, limit=3, cursors=cursors):
+                assert len(item) <= 2
+                break
+        assert str(e.value) == (
+            "Same cursors used twice. Please use a different set of cursors or start a new iteration. "
+            "This is to avoid accidental infinite loops."
+        )
