efeedf724d011314ad35547c06873e17b134fc90
anders-albert <anders0albert@gmail.com>
2025-06-15 16:01:08 +0200

    refactor: added helper used in local dev

--- tests/utils.py
+++ tests/utils.py
@@ -1,6 +1,16 @@
+import getpass
+import os
+import subprocess
 from collections.abc import Iterable
+from contextlib import redirect_stderr, redirect_stdout, suppress
+from dataclasses import dataclass, fields
+from pathlib import Path
+from typing import Literal, TypeAlias, get_args
 
-from cognite.client import data_modeling as dm, CogniteClient, ClientConfig
+from cognite.client import ClientConfig, CogniteClient
+from cognite.client import data_modeling as dm
+from cognite.client.config import global_config
+from cognite.client.credentials import CredentialProvider, OAuthClientCredentials, OAuthInteractive, Token
 
 from cognite.pygen._core.models import DataClass, EdgeDataClass, NodeDataClass
 from cognite.pygen.config import PygenConfig
@@ -31,32 +41,16 @@ def to_data_class_by_view_id(
 
     return node_by_view_id, edge_by_view_id
 
-import os
-import subprocess
-from contextlib import suppress
-from dataclasses import dataclass, fields
-from pathlib import Path
-from typing import Literal, TypeAlias, get_args
-
-from cognite.client.config import global_config
-from cognite.client.credentials import CredentialProvider, OAuthClientCredentials, OAuthInteractive, Token
-from rich.prompt import Prompt
-
-from cognite_toolkit._cdf_tk.client import ToolkitClient, ToolkitClientConfig
-from cognite_toolkit._cdf_tk.utils.auth import CLIENT_NAME
 
 _LOGIN_FLOW: TypeAlias = Literal["infer", "client_credentials", "interactive", "token"]
 _VALID_LOGIN_FLOWS = get_args(_LOGIN_FLOW)
 
 
 def get_cognite_client(env_file_name: str) -> CogniteClient:
-    """Instantiate a CogniteClient using environment variables. If the environment variables are not set, the user will
-    be prompted to enter them.
+    """Instantiate a CogniteClient using environment variables.
 
     Args:
-        env_file_name: The name of the .env file to look for in the repository root / current working directory. If
-        the file is found, the variables will be loaded from the file. If the file is not found, the user will
-        be prompted to enter the variables and the file will be created.
+        env_file_name: The name of the .env file to look for in the repository root / current working directory.
 
     Returns:
         CogniteClient: A CogniteClient instance.
@@ -86,32 +80,8 @@ def get_cognite_client(env_file_name: str) -> CogniteClient:
         variables = EnvironmentVariables.create_from_environ()
         print("Loaded variables from environment variables.")
         return variables.get_client()
-    # If not found, prompt the user
-    variables = _prompt_user()
-    if repo_root and _env_in_gitignore(repo_root, env_file_name):
-        env_file = repo_root / env_file_name
-        location = "repository root"
-    elif repo_root:
-        # We do not offer to create the file in the repository root if it is in .gitignore
-        # as an inexperienced user might accidentally commit it.
-        print("Cannot create .env file in repository root as there is no .env entry in the .gitignore.")
-        return variables.get_client()
-    else:
-        env_file = Path.cwd() / env_file_name
-        location = "current working directory"
 
-    answer = Prompt.ask(
-        f"Do you store the variables in an {env_file_name} file in the {location} for easy reuse?",
-        choices=["y", "n"],
-    )
-    if env_file.exists():
-        answer = Prompt.ask(f"{env_file} already exists. Overwrite?", choices=["y", "n"])
-    if answer == "y":
-        env_file.write_text(variables.create_env_file())
-        print(f"Created {env_file_name} file in {location}.")
-
-    return variables.get_client()
-
+    raise ValueError(f"Could not find {env_file_name} file in repository or current directory.")
 
 
 @dataclass
@@ -262,7 +232,7 @@ class EnvironmentVariables:
 
     def get_client(self) -> CogniteClient:
         config = ClientConfig(
-            client_name=CLIENT_NAME,
+            client_name=getpass.getuser(),
             project=self.CDF_PROJECT,
             credentials=self.get_credentials(),
             base_url=self.cdf_url,
@@ -305,77 +275,10 @@ def _from_dotenv(evn_file: Path) -> EnvironmentVariables:
     return EnvironmentVariables(**variables)  # type: ignore[arg-type]
 
 
-def _prompt_user() -> EnvironmentVariables:
-    try:
-        variables = EnvironmentVariables.create_from_environ()
-        continue_ = Prompt.ask(
-            f"Use environment variables for CDF Cluster '{variables.CDF_CLUSTER}' "
-            f"and Project '{variables.CDF_PROJECT}'? [y/n]",
-            choices=["y", "n"],
-            default="y",
-        )
-        if continue_ == "n":
-            variables = _prompt_cluster_and_project()
-    except KeyError:
-        variables = _prompt_cluster_and_project()
-
-    login_flow = Prompt.ask("Login flow", choices=[f for f in _VALID_LOGIN_FLOWS if f != "infer"])
-    variables.LOGIN_FLOW = login_flow  # type: ignore[assignment]
-    if login_flow == "token":
-        token = Prompt.ask("Enter token")
-        variables.TOKEN = token
-        return variables
-
-    variables.IDP_CLIENT_ID = Prompt.ask("Enter IDP Client ID")
-    if login_flow == "client_credentials":
-        variables.IDP_CLIENT_SECRET = Prompt.ask("Enter IDP Client Secret", password=True)
-        tenant_id = Prompt.ask("Enter IDP_TENANT_ID (leave empty to enter IDP_TOKEN_URL instead)")
-        if tenant_id:
-            variables.IDP_TENANT_ID = tenant_id
-        else:
-            token_url = Prompt.ask("Enter IDP_TOKEN_URL")
-            variables.IDP_TOKEN_URL = token_url
-        optional = ["IDP_AUDIENCE", "IDP_SCOPES"]
-    else:  # login_flow == "interactive"
-        tenant_id = Prompt.ask("Enter IDP_TENANT_ID (leave empty to enter IDP_AUTHORITY_URL instead)")
-        if tenant_id:
-            variables.IDP_TENANT_ID = tenant_id
-        else:
-            variables.IDP_AUTHORITY_URL = Prompt.ask("Enter IDP_TOKEN_URL")
-        optional = ["IDP_SCOPES"]
-
-    defaults = "".join(f"\n - {name}: {getattr(variables, name.lower())}" for name in optional)
-    use_defaults = Prompt.ask(
-        f"Use default values for the following variables?{defaults}", choices=["y", "n"], default="y"
-    )
-    if use_defaults:
-        return variables
-    for name in optional:
-        value = Prompt.ask(f"Enter {name}")
-        setattr(variables, name, value)
-    return variables
-
-
-def _prompt_cluster_and_project() -> EnvironmentVariables:
-    from rich.prompt import Prompt
-
-    cluster = Prompt.ask("Enter CDF Cluster (example 'greenfield', 'bluefield', 'westeurope-1)")
-    project = Prompt.ask("Enter CDF Project")
-    return EnvironmentVariables(cluster, project)
-
-
 def _repo_root() -> Path | None:
-    with suppress(Exception):
-        result = subprocess.run("git rev-parse --show-toplevel".split(), stdout=subprocess.PIPE)
-        if (output := result.stdout.decode().strip()) != "":
-            return Path(output)
+    with Path(os.devnull).open("w") as devnull:
+        with suppress(Exception), redirect_stdout(devnull), redirect_stderr(devnull):
+            result = subprocess.run("git rev-parse --show-toplevel".split(), stdout=subprocess.PIPE)
+            if (output := result.stdout.decode().strip()) != "":
+                return Path(output)
     return None
-
-
-def _env_in_gitignore(repo_root: Path, env_file_name: str) -> bool:
-    ignore_file = repo_root / ".gitignore"
-    if not ignore_file.exists():
-        return False
-    else:
-        ignored = {line.strip() for line in ignore_file.read_text().splitlines()}
-        return env_file_name in ignored or "*.env" in ignored
