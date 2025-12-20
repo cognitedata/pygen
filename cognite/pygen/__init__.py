799d90f4fa8d1db7d4bea42cfbb03b4991495e35
anders-albert <anders0albert@gmail.com>
2025-12-20 07:36:27 +0100

    refactor; added dummy

--- cognite/pygen/__init__.py
+++ cognite/pygen/__init__.py
@@ -4,3 +4,8 @@ This is the v2 rewrite of Pygen with improved architecture and multi-language su
 """
 
 __version__ = "2.0.0-dev"
+
+
+def generate_sdk() -> None:
+    """Placeholder function for SDK generation logic."""
+    raise NotImplementedError()
