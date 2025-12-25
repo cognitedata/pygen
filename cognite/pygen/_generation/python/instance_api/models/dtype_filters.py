d725d2e9f84971eb428adfb665900f28f7f18c6c
anders-albert <anders0albert@gmail.com>
2025-12-25 09:32:19 +0100

    refactor: improved implementation

--- cognite/pygen/_generation/python/instance_api/models/dtype_filters.py
+++ cognite/pygen/_generation/python/instance_api/models/dtype_filters.py
@@ -1,10 +1,14 @@
 import sys
-from abc import ABC, abstractmethod
 from datetime import date, datetime
 from typing import Any, Literal
 
 from ._references import ViewReference
-from .filters import Filter, FilterAdapter
+from .filters import (
+    Filter,
+    FilterAdapter,
+    FilterData,
+    FilterTypes,
+)
 
 if sys.version_info >= (3, 11):
     from typing import Self
@@ -12,18 +16,23 @@ else:
     from typing_extensions import Self
 
 
-class DataTypeFilter(ABC):
+class DataTypeFilter:
     """Base class for all data type filters."""
 
     def __init__(self, view_ref: ViewReference, property_id: str, operator: Literal["and", "or"]) -> None:
         self._view_ref = view_ref
         self._property_id = property_id
         self._operator = operator
-        self._conditions: list[tuple[str, Any]] = []
+        self._filters: dict[FilterTypes, FilterData] = {}
 
-    def _add_condition(self, operator: str, value: Any) -> Self:
+    def _add_filter(self, filter_type: FilterTypes, key: str, value: Any) -> Self:
         """Add a filter condition and return self for chaining."""
-        self._conditions.append((operator, value))
+        if filter_type in self._filters:
+            current_filter = self._filters[filter_type]
+            self._filters[filter_type] = current_filter.model_copy(update={key: value})
+        else:
+            filter_ = FilterAdapter.validate_python({filter_type: {"property": self._property_path, key: value}})
+            self._filters.update(filter_)
         return self
 
     @property
@@ -31,47 +40,16 @@ class DataTypeFilter(ABC):
         """Get the property path for the filter."""
         return [self._view_ref.space, f"{self._view_ref.external_id}/{self._view_ref.version}", self._property_id]
 
-    @abstractmethod
     def as_filter(self) -> Filter:
         """Convert the accumulated conditions to a Filter."""
-        ...
-
-    def _build_leaf_filter(self, operator: str, value: Any) -> dict[str, Any]:
-        """Build a single leaf filter dict."""
-        if operator == "eq":
-            return {"equals": {"property": self._property_path, "value": value}}
-        elif operator == "ne":
-            return {"not": {"equals": {"property": self._property_path, "value": value}}}
-        elif operator == "lt":
-            return {"range": {"property": self._property_path, "lt": value}}
-        elif operator == "le":
-            return {"range": {"property": self._property_path, "lte": value}}
-        elif operator == "gt":
-            return {"range": {"property": self._property_path, "gt": value}}
-        elif operator == "ge":
-            return {"range": {"property": self._property_path, "gte": value}}
-        elif operator == "prefix":
-            return {"prefix": {"property": self._property_path, "value": value}}
-        elif operator == "in":
-            return {"in": {"property": self._property_path, "values": value}}
-        else:
-            raise ValueError(f"Unknown operator: {operator}")
-
-    def _build_filter(self) -> Filter:
-        """Build the complete filter from accumulated conditions."""
-        from .filters import FilterAdapter
-
-        if not self._conditions:
+        if not self._filters:
             raise ValueError("No filter conditions have been added.")
 
-        if len(self._conditions) == 1:
-            operator, value = self._conditions[0]
-            filter_dict = self._build_leaf_filter(operator, value)
-        else:
-            leaf_filters = [self._build_leaf_filter(op, val) for op, val in self._conditions]
-            filter_dict = {self._operator: leaf_filters}
+        if len(self._filters) == 1:
+            return self._filters
 
-        return FilterAdapter.validate_python(filter_dict)
+        leaf_filters = [{filter_type: value} for filter_type, value in self._filters.items()]
+        return {self._operator: leaf_filters}  # type: ignore[dict-item]
 
 
 class FilterContainer:
@@ -101,31 +79,23 @@ class FloatFilter(DataTypeFilter):
 
     def equals(self, value: float | int) -> Self:
         """Filter for values equal to the given value."""
-        return self._add_condition("eq", self._validate_value(value))
-
-    def not_equals(self, value: float | int) -> Self:
-        """Filter for values not equal to the given value."""
-        return self._add_condition("ne", self._validate_value(value))
+        return self._add_filter("equals", "value", self._validate_value(value))
 
     def less_than(self, value: float | int) -> Self:
         """Filter for values less than the given value."""
-        return self._add_condition("lt", self._validate_value(value))
+        return self._add_filter("range", "lt", self._validate_value(value))
 
     def less_than_or_equals(self, value: float | int) -> Self:
         """Filter for values less than or equal to the given value."""
-        return self._add_condition("le", self._validate_value(value))
+        return self._add_filter("range", "le", self._validate_value(value))
 
     def greater_than(self, value: float | int) -> Self:
         """Filter for values greater than the given value."""
-        return self._add_condition("gt", self._validate_value(value))
+        return self._add_filter("range", "gt", self._validate_value(value))
 
     def greater_than_or_equals(self, value: float | int) -> Self:
         """Filter for values greater than or equal to the given value."""
-        return self._add_condition("ge", self._validate_value(value))
-
-    def as_filter(self) -> Filter:
-        """Convert the accumulated conditions to a Filter."""
-        return self._build_filter()
+        return self._add_filter("range", "ge", self._validate_value(value))
 
 
 class IntegerFilter(DataTypeFilter):
@@ -136,31 +106,23 @@ class IntegerFilter(DataTypeFilter):
 
     def equals(self, value: int) -> Self:
         """Filter for values equal to the given value."""
-        return self._add_condition("eq", self._validate_value(value))
-
-    def not_equals(self, value: int) -> Self:
-        """Filter for values not equal to the given value."""
-        return self._add_condition("ne", self._validate_value(value))
+        return self._add_filter("equals", "value", self._validate_value(value))
 
     def less_than(self, value: int) -> Self:
         """Filter for values less than the given value."""
-        return self._add_condition("lt", self._validate_value(value))
+        return self._add_filter("range", "lt", self._validate_value(value))
 
     def less_than_or_equals(self, value: int) -> Self:
         """Filter for values less than or equal to the given value."""
-        return self._add_condition("le", self._validate_value(value))
+        return self._add_filter("range", "le", self._validate_value(value))
 
     def greater_than(self, value: int) -> Self:
         """Filter for values greater than the given value."""
-        return self._add_condition("gt", self._validate_value(value))
+        return self._add_filter("range", "gt", self._validate_value(value))
 
     def greater_than_or_equals(self, value: int) -> Self:
         """Filter for values greater than or equal to the given value."""
-        return self._add_condition("ge", self._validate_value(value))
-
-    def as_filter(self) -> Filter:
-        """Convert the accumulated conditions to a Filter."""
-        return self._build_filter()
+        return self._add_filter("range", "ge", self._validate_value(value))
 
 
 class DateTimeFilter(DataTypeFilter):
@@ -177,31 +139,23 @@ class DateTimeFilter(DataTypeFilter):
 
     def equals(self, value: datetime | str) -> Self:
         """Filter for values equal to the given value."""
-        return self._add_condition("eq", self._validate_value(value))
-
-    def not_equals(self, value: datetime | str) -> Self:
-        """Filter for values not equal to the given value."""
-        return self._add_condition("ne", self._validate_value(value))
+        return self._add_filter("equals", "value", self._validate_value(value))
 
     def less_than(self, value: datetime | str) -> Self:
         """Filter for values less than the given value."""
-        return self._add_condition("lt", self._validate_value(value))
+        return self._add_filter("range", "lt", self._validate_value(value))
 
     def less_than_or_equals(self, value: datetime | str) -> Self:
         """Filter for values less than or equal to the given value."""
-        return self._add_condition("le", self._validate_value(value))
+        return self._add_filter("range", "le", self._validate_value(value))
 
     def greater_than(self, value: datetime | str) -> Self:
         """Filter for values greater than the given value."""
-        return self._add_condition("gt", self._validate_value(value))
+        return self._add_filter("range", "gt", self._validate_value(value))
 
     def greater_than_or_equals(self, value: datetime | str) -> Self:
         """Filter for values greater than or equal to the given value."""
-        return self._add_condition("ge", self._validate_value(value))
-
-    def as_filter(self) -> Filter:
-        """Convert the accumulated conditions to a Filter."""
-        return self._build_filter()
+        return self._add_filter("range", "ge", self._validate_value(value))
 
 
 class DateFilter(DataTypeFilter):
@@ -218,31 +172,23 @@ class DateFilter(DataTypeFilter):
 
     def equals(self, value: date | str) -> Self:
         """Filter for values equal to the given value."""
-        return self._add_condition("eq", self._validate_value(value))
-
-    def not_equals(self, value: date | str) -> Self:
-        """Filter for values not equal to the given value."""
-        return self._add_condition("ne", self._validate_value(value))
+        return self._add_filter("equals", "value", self._validate_value(value))
 
     def less_than(self, value: date | str) -> Self:
         """Filter for values less than the given value."""
-        return self._add_condition("lt", self._validate_value(value))
+        return self._add_filter("range", "lt", self._validate_value(value))
 
     def less_than_or_equals(self, value: date | str) -> Self:
         """Filter for values less than or equal to the given value."""
-        return self._add_condition("le", self._validate_value(value))
+        return self._add_filter("range", "le", self._validate_value(value))
 
     def greater_than(self, value: date | str) -> Self:
         """Filter for values greater than the given value."""
-        return self._add_condition("gt", self._validate_value(value))
+        return self._add_filter("range", "gt", self._validate_value(value))
 
     def greater_than_or_equals(self, value: date | str) -> Self:
         """Filter for values greater than or equal to the given value."""
-        return self._add_condition("ge", self._validate_value(value))
-
-    def as_filter(self) -> Filter:
-        """Convert the accumulated conditions to a Filter."""
-        return self._build_filter()
+        return self._add_filter("range", "ge", self._validate_value(value))
 
 
 class TextFilter(DataTypeFilter):
@@ -250,19 +196,15 @@ class TextFilter(DataTypeFilter):
 
     def equals(self, value: str) -> Self:
         """Filter for values equal to the given string."""
-        return self._add_condition("eq", str(value))
+        return self._add_filter("equals", "value", str(value))
 
     def prefix(self, value: str) -> Self:
         """Filter for values starting with the given prefix."""
-        return self._add_condition("prefix", str(value))
+        return self._add_filter("prefix", "value", str(value))
 
     def in_(self, values: list[str]) -> Self:
         """Filter for values that are in the given list."""
-        return self._add_condition("in", [str(v) for v in values])
-
-    def as_filter(self) -> Filter:
-        """Convert the accumulated conditions to a Filter."""
-        return self._build_filter()
+        return self._add_filter("in", "values", [str(v) for v in values])
 
 
 class BooleanFilter(DataTypeFilter):
@@ -270,8 +212,4 @@ class BooleanFilter(DataTypeFilter):
 
     def equals(self, value: bool) -> Self:
         """Filter for values equal to the given boolean."""
-        return self._add_condition("eq", bool(value))
-
-    def as_filter(self) -> Filter:
-        """Convert the accumulated conditions to a Filter."""
-        return self._build_filter()
+        return self._add_filter("equals", "value", bool(value))
