7b761dfbbc88870c07ad2c09dc4fb74ba66f2568
anders-albert <anders0albert@gmail.com>
2025-12-28 20:56:16 +0100

    style: formatting

--- cognite/pygen/_generation/typescript/instance_api/types/filters.ts
+++ cognite/pygen/_generation/typescript/instance_api/types/filters.ts
@@ -136,8 +136,10 @@ export interface OverlapsFilter {
 
 /** HasData filter - instance has data in specified views/containers */
 export interface HasDataFilter {
-  readonly hasData: (| ({ readonly type: "view" } & ViewReference)
-    | ({ readonly type: "container" } & ContainerReference))[];
+  readonly hasData: (
+    | ({ readonly type: "view" } & ViewReference)
+    | ({ readonly type: "container" } & ContainerReference)
+  )[];
 }
 
 /** InstanceReferences filter - matches specific instance IDs */
