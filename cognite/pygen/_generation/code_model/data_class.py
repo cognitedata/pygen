2f82b96e0b5c424a5f80db3ba6138c70d84ab9d2
anders-albert <anders0albert@gmail.com>
2025-12-30 21:14:48 +0100

    added missing

--- cognite/pygen/_generation/code_model/data_class.py
+++ cognite/pygen/_generation/code_model/data_class.py
@@ -1,3 +1,5 @@
+from typing import Literal
+
 from cognite.pygen._client.models import ViewReference
 
 from .field import Field
@@ -8,6 +10,7 @@ class DataClass(CodeModel):
     view_id: ViewReference
     name: str
     fields: list[Field]
+    instance_type: Literal["node", "edge"]
 
 
 class ReadDataClass(DataClass):
