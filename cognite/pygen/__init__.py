31099455c50eb57623e1fce2c8fbb2f8daa4083c
anders-albert <anders0albert@gmail.com>
2025-12-31 06:47:47 +0100

    refactor: renaming

--- cognite/pygen/__init__.py
+++ cognite/pygen/__init__.py
@@ -3,8 +3,8 @@ This is the main entry point for the pygen package. It contains the main functio
 generating SDKs.
 """
 
-from cognite.pygen._generator.generator_functions import generate_sdk as generate_sdk_v2
-from cognite.pygen._generator.generator_functions import generate_sdk_notebook as generate_sdk_notebook_v2
+from cognite.pygen._generator.gen_functions import generate_sdk as generate_sdk_v2
+from cognite.pygen._generator.gen_functions import generate_sdk_notebook as generate_sdk_notebook_v2
 from cognite.pygen._legacy._build import build_wheel
 from cognite.pygen._legacy._generator import generate_sdk, generate_sdk_notebook
 from cognite.pygen._legacy._query import QueryExecutor as _QueryExecutor
