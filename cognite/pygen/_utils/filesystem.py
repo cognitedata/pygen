c4ebffb68daffeb5168c02336721dfc007d7aaea
anders-albert <anders0albert@gmail.com>
2026-01-01 07:49:12 +0100

    fix: improvment

--- cognite/pygen/_utils/filesystem.py
+++ cognite/pygen/_utils/filesystem.py
@@ -3,13 +3,11 @@ import re
 
 def sanitize(name: str) -> str:
     """Sanitize a string to be used as a file or directory name."""
-    # Characters to remove completely (invalid in filenames)
-    result = re.sub(r'[<>"/\\|?*]', '', name)
-    # Characters to replace with underscore
-    result = re.sub(r'[: ]', '_', result)
+    result = re.sub(r'[<>"/\\|?*]', "", name)
+    result = re.sub(r"[: ]", "_", result)
+    result = re.sub("_+", "_", result)
 
-    # Handle trailing dots - strip all and add underscore + single dot
-    if result.endswith('.'):
-        result = result.rstrip('.') + '_.'
+    if result.endswith("."):
+        result = result.rstrip(".") + "_."
 
     return result
