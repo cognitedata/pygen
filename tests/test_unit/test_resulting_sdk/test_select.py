5cca73a9d9ca42b70e8049e4f342d0992571c328
Morten Grønbech <morten.gronbech@cognite.com>
2026-02-06 09:08:54 +0100

    Be more specific when filtering the warning in the unit test

--- tests/test_unit/test_resulting_sdk/test_select.py
+++ tests/test_unit/test_resulting_sdk/test_select.py
@@ -4,7 +4,9 @@ from omni import OmniClient
 
 
 class TestSelectMethod:
-    @pytest.mark.filterwarnings("error")
+    @pytest.mark.filterwarnings(
+        r"error:You may be trying to combine two \(or more\) filters using 'and' or 'or'.*:UserWarning"
+    )
     def test_dump_yaml(self) -> None:
         with monkeypatch_cognite_client() as client:
             pygen = OmniClient(client)
