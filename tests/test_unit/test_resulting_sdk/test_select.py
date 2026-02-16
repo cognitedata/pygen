14b3d580eac89130c919211bf9a2a6eb695b103f
Morten Grønbech <42722577+MortGron@users.noreply.github.com>
2026-02-16 12:08:25 +0100

    fix: Remove warning when using the select method (#617)
    # Description
    
    **Reviewer**: All code inside `examples/` is generated.
    
    Using the `select()` method would lead to this warning:
    <img width="1168" height="241" alt="image"
    src="https://github.com/user-attachments/assets/8939bef0-0062-42e3-aa8e-981f76925617"
    />
    
    ## Bump
    
    - [x] Patch
    - [ ] Minor
    - [ ] Skip
    
    ## Changelog
    ### Fixed
    
    - Using the `select` method no longer generates a warning about
    incorrect use of filters.

--- tests/test_unit/test_resulting_sdk/test_select.py
+++ tests/test_unit/test_resulting_sdk/test_select.py
@@ -1,8 +1,12 @@
+import pytest
 from cognite.client.testing import monkeypatch_cognite_client
 from omni import OmniClient
 
 
 class TestSelectMethod:
+    @pytest.mark.filterwarnings(
+        r"error:You may be trying to combine two \(or more\) filters using 'and' or 'or'.*:UserWarning"
+    )
     def test_dump_yaml(self) -> None:
         with monkeypatch_cognite_client() as client:
             pygen = OmniClient(client)
