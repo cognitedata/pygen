9da70aa6446a9aea5d78bf0d5e5e4fc1da8581cd
anders-albert <anders0albert@gmail.com>
2026-09-26 11:52:39 +0200

    tests: first pass on test

--- tests/test_integration/test_query_execution.py
+++ tests/test_integration/test_query_execution.py
@@ -153,6 +153,16 @@ def test_query_list_primitive_properties(cognite_client: CogniteClient, omni_vie
     assert not ill_formed_items, f"Items with unexpected properties: {ill_formed_items}"
 
 
+def test_query_list_naughty_view(cognite_client: CogniteClient, omni_views: dict[str, dm.View]) -> None:
+    view = omni_views["Naughty"]
+
+    executor = QueryExecutor(cognite_client, views=[view])
+
+    result = executor.list(view.as_id(), ["externalId", "type"], limit=5)
+    result_with_type = [item for item in result if "type" in item]
+    assert len(result_with_type) > 0
+
+
 @pytest.fixture()
 def connection_item_e_with_source(omni_client: OmniClient) -> tuple[dc.ConnectionItemEWrite, dc.PrimitiveNullableWrite]:
     target = dc.PrimitiveNullableWrite(
